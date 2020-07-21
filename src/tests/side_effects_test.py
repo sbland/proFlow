efeb0c5a52b005c0a70d4a4ab20abad32da36d84
sbland <github.com/sbland>
2020-07-21 15:27:50 +0100

    move side effects mocks to mocks file

--- src/tests/side_effects_test.py
+++ src/tests/side_effects_test.py
@@ -3,55 +3,13 @@ from unittest.mock import MagicMock, Mock, patch
 import pytest
 from dataclasses import dataclass, field
 from vendor.helpers.list_helpers import flatten_list
-
+from src.tests.mocks import Mock_Config_Shape, Mock_External_State_Shape, \
+    Mock_Model_State_Shape, Mock_Nested_State, Mock_Parameters_Shape
 from ..ProcessRunner import Process, I
 
 
 def process_add(x, y):
     return x + y
-
-
-@dataclass
-class Mock_Nested_State:
-    na: int = 7
-    nab: int = 7
-
-
-@dataclass
-class Mock_Model_State_Shape:
-    a: float
-    b: float
-    c: float = 0
-    d: float = 0
-    target: str = "a"
-    lst: list = None
-    nested: Mock_Nested_State = Mock_Nested_State()
-
-
-@dataclass
-class Mock_Config_Shape:
-    foo: int = 1
-    bar: int = 3
-    roo: dict = field(default_factory=lambda: {
-        'abc': 5
-    })
-    arr: list = field(default_factory=lambda: [1, 2, 3])
-
-
-@dataclass
-class Mock_Parameters_Shape:
-    foo: int = 1
-    bar: int = 3
-    roo: dict = field(default_factory=lambda: {
-        'abc': 5
-    })
-    arr: list = field(default_factory=lambda: [1, 2, 3])
-
-
-@dataclass
-class Mock_External_State_Shape:
-    data_a: int = 1
-    data_b: int = 5
 
 
 process_runner = ProcessRunner(
