b318d396b9a55641cf86a7085052ccf98463f3a5
sbland <github.com/sbland>
2020-07-21 11:15:37 +0100

    implement wildcard in get nested vals

--- vendor/helpers/dictionary_helpers.py
+++ vendor/helpers/dictionary_helpers.py
@@ -1,12 +1,22 @@
 from dataclasses import is_dataclass
-from functools import reduce
 
 import numpy as np
 
 from .comparisons import isNamedTuple
 
 
-def get_nested_arg_from_dict(data: dict, location_str: str):
+def get_val_from_obj(obj, k):
+    # TODO: Check object type
+    val = None
+    val = obj[k] if isinstance(obj, dict) else val  # dictionary access
+    val = obj[int(k)] if isinstance(obj, list) else val  # list access
+    val = obj[int(k)] if isinstance(obj, np.ndarray) else val  # numpy array access
+    val = obj._asdict()[k] if isNamedTuple(obj) else val  # namedtuple access
+    val = getattr(obj, k) if is_dataclass(obj) else val  # dataclass access
+    return val
+
+
+def get_nested_val(data: dict, location_str: str):
     """ Helper function to get a value from a dictionary
     when given a dot notation string
 
@@ -21,31 +31,35 @@ def get_nested_arg_from_dict(data: dict, location_str: str):
                 'sow': 10,
                 }
             },
-        'arr': [1, 2, 3],
         'a': A(),
+        'arr': [1, 2, 3],
+        'deepmatrix': [[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+        'dictlist': [{'foo': 'abc'}, {'foo': 'def'}],
         }
     ```
 
     ```
-    result = get_nested_arg_from_dict(data, 'bar.ree.sow')
+    result = get_nested_val(data, 'bar.ree.sow')
     assert result == 10
-    result = get_nested_arg_from_dict(data, 'arr.1')
+    result = get_nested_val(data, 'arr.1')
     assert result == 2
+
+    ## Wildcards
+    result = get_nested_val(data, 'deepmatrix._.1')
+    assert result == []
     ```
     """
-    location = location_str.split('.')
-
-    def get_val(acc, k):
-        val = None
-        val = acc[k] if isinstance(acc, dict) else val
-        val = acc[int(k)] if isinstance(acc, list) else val
-        val = acc[int(k)] if isinstance(acc, np.ndarray) else val
-        val = acc._asdict()[k] if isNamedTuple(acc) else val
-        val = getattr(acc, k) if is_dataclass(acc) else val
-        if val is None:
-            print(acc)
-            raise ValueError('Value not found in object')
-        return val
-        # TODO: Optimize
-    result = reduce(get_val, location, data)
-    return result
+    loc_split = location_str.split('.')
+    loc_arr = [loc_split[i:] for i in range(0, len(loc_split))]
+    out = data
+    for i in range(len(loc_arr)):
+        k = loc_arr[i][0]
+        if k != '_':
+            out = get_val_from_obj(out, k)
+        if k == '_':
+            if i == len(loc_arr) - 1:
+                out = out
+            else:
+                out = [get_nested_val(o, '.'.join(loc_arr[i+1])) for o in out]
+            break
+    return out
--- vendor/helpers/dictionary_helpers_test.py
+++ vendor/helpers/dictionary_helpers_test.py
@@ -1,7 +1,8 @@
+
 from typing import NamedTuple
 
 from .fill_np_array import fill_np_array_with_cls
-from .dictionary_helpers import get_nested_arg_from_dict
+from .dictionary_helpers import get_nested_val
 
 
 def test_get_nested_args_from_dict():
@@ -19,13 +20,13 @@ def test_get_nested_args_from_dict():
         'arr': [1, 2, 3],
         'a': A(),
     }
-    result = get_nested_arg_from_dict(data, 'bar.roo')
+    result = get_nested_val(data, 'bar.roo')
     assert result == 4
-    result = get_nested_arg_from_dict(data, 'bar.ree.sow')
+    result = get_nested_val(data, 'bar.ree.sow')
     assert result == 10
-    result = get_nested_arg_from_dict(data, 'arr.1')
+    result = get_nested_val(data, 'arr.1')
     assert result == 2
-    result = get_nested_arg_from_dict(data, 'a.val')
+    result = get_nested_val(data, 'a.val')
     assert result == 1
 
 
@@ -40,5 +41,40 @@ def test_get_nested_args_with_numpy_list():
     data = {
         'a': A(),
     }
-    result = get_nested_arg_from_dict(data, 'a.sub.0.name')
+    result = get_nested_val(data, 'a.sub.0.name')
     assert result == 'dave'
+
+
+def test_get_nested_arg_from_list():
+    class A(NamedTuple):
+        val: int = 1
+
+    data = {
+        'foo': 1,
+        'arr': [1, 2, 3],
+        'matrix': [[1, 2, 3], [4, 5, 6]]
+    }
+    result = get_nested_val(data, 'arr.1')
+    assert result == 2
+    result = get_nested_val(data, 'matrix.1.1')
+    assert result == 5
+
+
+def test_get_nested_arg_from_list_with_wildcard():
+    class A(NamedTuple):
+        val: int = 1
+
+    data = {
+        'foo': 1,
+        'arr': [1, 2, 3],
+        'matrix': [[1, 2, 3], [4, 5, 6]],
+        'deepmatrix': [[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+        'dictlist': [{'foo': 'abc'}, {'foo': 'def'}]
+    }
+    # result = get_nested_val(data, 'matrix.1.x')
+    # assert result == [4, 5, 6]
+    result = get_nested_val(data, 'matrix._.1')
+    assert result == [2, 5]
+    result = get_nested_val(data, 'deepmatrix._.1._')
+    assert result == [[3, 4], [7, 8]]
+    result = get_nested_val(data, 'dictlist._.foo')
