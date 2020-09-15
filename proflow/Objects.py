75d5b2da9235ea403e31904b8535ab1ddfe3ec94
sam_bland <sbland.co.uk@gmail.com>
2020-09-15 16:44:38 +0100

    Tidy up Process Object

--- proflow/Objects.py
+++ proflow/Objects.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass, field
+from proflow.external_state import External_State_Shape
+from proflow.parameters import Parameters_Shape
+from proflow.config import Config_Shape
 from proflow.process_inspector import parse_inputs
 from typing import Callable, List
 
@@ -27,18 +30,18 @@ class Process:
     group: str = None  # group tag
     # Inputs to function
     # TODO: The below objects could be set as the actual State Shape objects instead
-    state_inputs: Callable[[object], List[I]] = \
-        field(default_factory=lambda: lambda *args, **kwargs: [])
-    config_inputs: Callable[[object], List[I]] = \
-        field(default_factory=lambda: lambda *args, **kwargs:  [])
-    parameters_inputs: Callable[[object], List[I]] = \
-        field(default_factory=lambda: lambda *args, **kwargs:  [])
-    external_state_inputs: Callable[[object], List[I]] = \
-        field(default_factory=lambda: lambda *args, **kwargs:  [])
-    additional_inputs: List[tuple] = \
-        field(default_factory=lambda: lambda *args, **kwargs:  [])
+    state_inputs: Callable[[Model_State_Shape], List[I]] = \
+        field(default_factory=lambda: lambda state: [])
+    config_inputs: Callable[[Config_Shape], List[I]] = \
+        field(default_factory=lambda: lambda config: [])
+    parameters_inputs: Callable[[Parameters_Shape], List[I]] = \
+        field(default_factory=lambda: lambda params: [])
+    external_state_inputs: Callable[[External_State_Shape, int], List[I]] = \
+        field(default_factory=lambda: lambda e_state, row_index: [])
+    additional_inputs: Callable[[], List[I]] = \
+        field(default_factory=lambda: lambda: [])
     # TODO: state_outputs is depreciated
-    state_outputs: Callable[[any, object], object] = field(default_factory=list)
+    state_outputs: List[I] = field(default_factory=list)
     map_outputs: Callable[[any, object], object] = field(default_factory=list)
     args: List[any] = field(default_factory=list)  # additional args
 
@@ -48,10 +51,6 @@ class Process:
             f'comment="{self.comment}"',
             f'gate={self.gate}',
             f'group={self.group}',
-            # f'config_inputs={self.config_inputs()}',
-            # f'parameters_inputs={self.parameters_inputs}',
-            # f'external_state_inputs={self.external_state_inputs}',
-            # f'additional_inputs={self.additional_inputs}',
             f'config_inputs={parse_inputs(self.config_inputs)}',
             f'parameters_inputs={parse_inputs(self.parameters_inputs)}',
             f'external_state_inputs={parse_inputs(self.external_state_inputs)}',
@@ -59,5 +58,4 @@ class Process:
             f'state_inputs={parse_inputs(self.state_inputs)}',
             f'state_outputs={self.state_outputs}',
             f'args={self.args}',
-
         ]) + ')'
