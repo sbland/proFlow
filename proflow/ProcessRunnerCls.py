f274b374fddca0035de49930844754cab29211f5
sam_bland <sbland.co.uk@gmail.com>
2020-09-16 20:00:15 +0100

    Implement new run process in process runner cls

--- proflow/ProcessRunnerCls.py
+++ proflow/ProcessRunnerCls.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 from functools import partial, reduce
-from proflow.parameters import Parameters_Shape
-from proflow.external_state import External_State_Shape
-from proflow.config import Config_Shape
+from proflow.process_ins_and_outs import get_inputs_from_process, map_result_to_state
 from typing import Callable, List, NamedTuple
-from .ProcessRunner import Process, run_process
+
+from .parameters import Parameters_Shape
+from .external_state import External_State_Shape
+from .config import Config_Shape
+from .ProcessRunner import Process
+from .errors import Run_Process_Error
 
 
 class ProcessRunner():
@@ -18,6 +22,8 @@ class ProcessRunner():
         self.parameters = parameters_in
         self.external_state = external_state_in
         self.DEBUG_MODE = DEBUG_MODE
+        self.time_logs = []
+        self.debug_time_logs = []
 
     # Define the process runner
     def run_processes(
@@ -32,7 +38,7 @@ class ProcessRunner():
         the reduce function allows us to iterate through each function
         passing the state to the next
         """
-        run_process_loaded = partial(run_process, config=self.config,
+        run_process_loaded = partial(self.run_process, config=self.config,
                                      parameters=self.parameters,
                                      external_state=self.external_state,
                                      DEBUG_MODE=self.DEBUG_MODE)
@@ -46,3 +52,102 @@ class ProcessRunner():
         """HOC component to assign processes to the run_processes function
         which can then be ran later with the state"""
         return partial(self.run_processes, processes)
+
+    def run_process(
+        self,
+        prev_state: NamedTuple,  # Can be state or parameter
+        process: Process,
+        config: Config_Shape,
+        parameters: Parameters_Shape,
+        external_state: External_State_Shape,
+        DEBUG_MODE: bool = False,
+    ) -> NamedTuple:
+        """Run a single process and output the updated state.
+            The process object contains the function along with all the input
+            and output targets.
+
+
+            note: args from process are not garuanteed to be in the correct order
+
+        Parameters
+        ----------
+        prev_state : NamedTuple
+            Model state prior to this process being ran
+        config : Config_Shape
+            Model configuration
+        parameters : Parameters_Shape
+            Model derived parameters
+        external_state : External_State_Shape
+            External data
+        DEBUG_MODE : bool, optional
+            Debug processes when True, by default False
+
+        Returns
+        -------
+        Model State
+            Model State after process
+
+        Raises
+        ------
+        Run_Process_Error
+            Catches error that occur when running the process
+        """
+        if not process.gate:
+            return prev_state
+        try:
+            process_id = process.comment or getattr(process.func, '__name__', 'Unknown')
+            if DEBUG_MODE:
+                start_time_input_setup = datetime.now()
+
+            args, kwargs = get_inputs_from_process(
+                process,
+                prev_state,
+                config,
+                parameters,
+                external_state,
+            )
+
+            if DEBUG_MODE:
+                end_time_input_setup = datetime.now()
+                time_diff_input_setup = (end_time_input_setup - start_time_input_setup)
+                execution_time_input_setup = time_diff_input_setup.total_seconds() * 1000
+
+            # RUN PROCESS FUNC
+            result = None
+            if DEBUG_MODE:
+                # Log time taken for process
+                start_time = datetime.now()
+                result = process.func(*args, **kwargs)
+                end_time = datetime.now()
+                time_diff = (end_time - start_time)
+                execution_time = time_diff.total_seconds() * 1000
+
+                self.time_logs.append((process_id, execution_time))
+            else:
+                result = process.func(*args, **kwargs)
+
+            if DEBUG_MODE:
+                start_time_output_setup = datetime.now()
+
+            output_state = map_result_to_state(prev_state, process.state_outputs, result)
+
+            if DEBUG_MODE:
+                end_time_output_setup = datetime.now()
+                time_diff_output_setup = (end_time_output_setup - start_time_output_setup)
+                execution_time_output_setup = time_diff_output_setup.total_seconds() * 1000
+
+            if DEBUG_MODE:
+                self.debug_time_logs.append({
+                    "id": process_id,
+                    "input_time": execution_time_input_setup,
+                    "output_time": execution_time_output_setup,
+                })
+
+            return output_state
+
+        except Exception as e:
+            raise Run_Process_Error(process, e, prev_state) from e
+
+    def reset_logs(self):
+        self.time_logs = []
+        self.debug_time_logs = []
---
+++ proflow/tests/time_logs_test.py
@@ -0,0 +1,176 @@
+import pytest
+
+from proflow.tests.mocks import Mock_Config_Shape, Mock_External_State_Shape, \
+    Mock_Model_State_Shape, Mock_Parameters_Shape
+from proflow.ProcessRunnerCls import ProcessRunner
+from unittest.mock import patch
+from vendor.helpers.list_helpers import flatten_list
+
+from ..ProcessRunner import Process, I
+
+
+def process_add(x, y):
+    return x + y
+
+
+def process_add_complex(x, y, *args, **kwargs):
+    total = 0
+    for i in range(10000):
+        total += x + y + i
+    return total
+
+
+process_runner = ProcessRunner(
+    Mock_Config_Shape(),
+    Mock_External_State_Shape(),
+    Mock_Parameters_Shape(),
+    True,
+)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def _():
+    with patch('proflow.ProcessRunner.Model_State_Shape', side_effect=Mock_Model_State_Shape) \
+            as Mocked_State_Shape:
+        Mocked_State_Shape.__annotations__ = Mock_Model_State_Shape.__annotations__
+        yield Mocked_State_Shape
+
+
+@pytest.fixture(scope="module", autouse=True)
+def __():
+    with patch('proflow.ProcessRunner.Config_Shape', return_value=Mock_Config_Shape) as _fixture:
+        yield _fixture
+
+
+@pytest.fixture(scope="module", autouse=True)
+def ____():
+    with patch('proflow.ProcessRunner.Parameters_Shape', return_value=Mock_Parameters_Shape) \
+            as _fixture:
+        yield _fixture
+
+
+@pytest.fixture(scope="module", autouse=True)
+def ______():
+    with patch('proflow.ProcessRunner.External_State_Shape',
+               return_value=Mock_External_State_Shape) \
+            as _fixture:
+        yield _fixture
+
+
+def test_that_processRunnerCls_logs_time_for_each_process():
+    process_runner.reset_logs()
+    state = Mock_Model_State_Shape(a=2.1, b=4.1)
+    processes = flatten_list([
+        Process(
+            func=process_add,
+            config_inputs=lambda config: [
+                I(config.foo, as_='x'),
+                I(config.bar, as_='y'),
+            ],
+            state_outputs=[
+                I('_result', as_='c'),
+            ],
+        ),
+        Process(
+            func=process_add_complex,
+            config_inputs=lambda config: [
+                I(config.foo, as_='x'),
+            ],
+            state_inputs=lambda state: [
+                I(state.a, as_='y'),
+            ],
+            state_outputs=[
+                I('_result', as_='d'),
+            ],
+        ),
+    ])
+    run_processes = process_runner.initialize_processes(processes)
+    state_2 = run_processes(initial_state=state)
+    assert state_2.c == 4
+    assert state_2.d == 50026000.00000531
+    time_logs = dict(process_runner.time_logs)
+    assert time_logs['process_add'] < time_logs['process_add_complex']
+
+
+def test_that_processRunnerCls_debug_logs_time_for_each_process():
+    process_runner.reset_logs()
+    state = Mock_Model_State_Shape(a=2.1, b=4.1)
+    processes = flatten_list([
+        Process(
+            func=process_add,
+            config_inputs=lambda config: [
+                I(config.foo, as_='x'),
+                I(config.bar, as_='y'),
+            ],
+            state_outputs=[
+                I('_result', as_='c'),
+            ],
+        ),
+        Process(
+            func=process_add_complex,
+            config_inputs=lambda config: [
+                I(config.foo, as_='x'),
+                I(config.foo, as_='x'),
+            ],
+            state_inputs=lambda state: [
+                I(state.a, as_='y'),
+                I(state.nested.na, as_='y1'),
+                I(state.nested.na, as_='y2'),
+                I(state.nested.na, as_='y3'),
+                I(state.nested.na, as_='y4'),
+                I(state.nested.na, as_='y5'),
+                I(state.nested.na, as_='y6'),
+                I(state.nested.na, as_='y7'),
+                I(state.nested.na, as_='y8'),
+                I(state.nested.na, as_='y9'),
+                I(state.nested.na, as_='y10'),
+                I(state.nested.na, as_='y11'),
+                I(state.nested.na, as_='y12'),
+                I(state.nested.na, as_='y13'),
+                I(state.nested.na, as_='y14'),
+                I(state.nested.na, as_='y15'),
+                I(state.nested.na, as_='y16'),
+                I(state.nested.na, as_='y17'),
+                I(state.nested.na, as_='y18'),
+                I(state.nested.na, as_='y19'),
+                I(state.nested.na, as_='y110'),
+                I(state.nested.na, as_='y111'),
+                I(state.nested.na, as_='y112'),
+                I(state.nested.na, as_='y113'),
+                I(state.nested.na, as_='y114'),
+                I(state.nested.na, as_='y115'),
+                I(state.nested.na, as_='y116'),
+                I(state.nested.na, as_='y117'),
+                I(state.nested.na, as_='y118'),
+                I(state.nested.na, as_='y119'),
+                I(state.nested.na, as_='y120'),
+                I(state.nested.na, as_='y121'),
+            ],
+            state_outputs=[
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+                I('_result', as_='nested.na'),
+            ],
+        ),
+    ])
+    run_processes = process_runner.initialize_processes(processes)
+    state_2 = run_processes(initial_state=state)
+    assert state_2.c == 4
+    debug_time_logs = process_runner.debug_time_logs
+    assert debug_time_logs[1]['input_time'] < 0.083
+    assert debug_time_logs[1]['output_time'] < 0.21
