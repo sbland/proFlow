08b2c3e28d4fe30fe8522a5f303184bd2137f6af
sam_bland <sbland.co.uk@gmail.com>
2020-09-16 19:21:18 +0100

    Fix error handling when cannot get __name__

--- proflow/errors.py
+++ proflow/errors.py
@@ -1,13 +1,14 @@
 class Run_Process_Error(Exception):
     def __init__(self, process: 'Process', error: Exception, state):
-        self.message = f'Failed to run {process.comment or process.func.__name__}'
+        process_id = process.comment or getattr(process.func, '__name__', 'Unknown')
+        self.message = f'Failed to run {process_id}'
         self.error = error
         self.state = state
 
     def __str__(self):
-        state_str = str(self.state)
-        state_print = state_str[0:100] + '...' + \
-            state_str[:-100] if len(state_str) > 200 else state_str
+        # state_str = str(self.state)
+        # state_print = state_str[0:100] + '...' + \
+        #     state_str[:-100] if len(state_str) > 200 else state_str
         return f"""{self.message}
         !!--------------!!
         {str(self.error)}
