096fc1294c86d1c1f5a624da694390f1435e76cf
Sam Bland <sam.bland@york.ac.uk>
2021-10-15 00:14:40 +0100

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="proflow",
-    version="0.3.6",
+    version="0.3.7",
     author="Sam Bland",
     author_email="sbland.co.uk@gmail.com",
     description="Functional Process Runner",
